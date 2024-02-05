1f8fe231b998311df6794c7ccd78f9b7a1f39fdc
Daniel Borrome <dborrome@gmail.com>
2024-02-05 16:18:34 -0400

    Añadir menú compartido y cambiar tema a oscuro en estilos.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { ButtonModule } from 'primeng/button';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    ButtonModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    SharedModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,6 @@
+import { NgModule } from '@angular/core';
+
+import { MenubarModule } from 'primeng/menubar';
+
+@NgModule({ exports: [MenubarModule] })
+export class PrimeNgModule {}
