2b9bd4bdafe0dae2fbeabffcc5816a732033fbcb
Daniel Borrome <dborrome@gmail.com>
2024-02-09 15:33:46 -0400

    Agregados pipes y funcionalidad de cambio de cliente en uncommon-page.component.ts

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-uncommon-page',
   templateUrl: './uncommon-page.component.html',
-  styleUrl: './uncommon-page.component.css'
+  styleUrl: './uncommon-page.component.css',
 })
 export class UncommonPageComponent {
+  // i18n Select Pipe
+  public name: string = 'Daniel';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap: { [key: string]: string } = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
 
+  changeClient(): void {
+    this.name = 'Melissa';
+    this.gender = 'female';
+  }
 }
