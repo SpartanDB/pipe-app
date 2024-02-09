55cea747da5352c37d7676dcfd53124da09b8287
Daniel Borrome <dborrome@gmail.com>
2024-02-09 16:46:18 -0400

    Agregado componente de orden en la página de productos

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
+import { OrderComponent } from './pages/order/order.component';
 import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 
 import { ProductsRoutingModule } from './products-routing.module';
@@ -12,6 +13,7 @@ import { PrimeNgModule } from '../prime-ng/prime-ng.module';
   declarations: [
     BasicsPageComponent,
     NumbersPageComponent,
+    OrderComponent,
     UncommonPageComponent,
   ],
   imports: [CommonModule, PrimeNgModule, ProductsRoutingModule],
